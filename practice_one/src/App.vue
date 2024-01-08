2633fad53b8dff3c147ca2ca5460b8e3c687f8bd
Michael Khanda <michaelkhanda006@gmail.com>
2024-01-08 10:12:57 +0300

    update

---
+++ practice_one/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+
+const header = ref('Shopping List App')
+const items = ref([
+  {id: 1, label: "10 party hats"},
+  {id: 2, label:"2 board games"},
+  {id: 3, label: "20 cups"}
+])
+const newItem = ref("")
+const newItemHighPriority = ref(false)
+const icecreamFlavours = ref([])
+</script>
+
+<template>
+  <h1>{{ header }}</h1>
+  <input v-model="newItem" type="text" placeholder="Add an item">
+<!--  {{newItem}}-->
+
+  <label>
+    <input type="checkbox" v-model="newItemHighPriority">
+    High Priority
+  </label>
+
+  <ul>
+    <li v-for="{id, label} in items" :key="id">
+      {{label}}
+    </li>
+  </ul>
+</template>
